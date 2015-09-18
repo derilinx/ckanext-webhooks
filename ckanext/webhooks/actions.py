149063f9b53a3188179f12d42f3cf9bfca25c6cf
Carl Lange <carl@flax.ie>
2015-09-18 12:57:20 +0100

    2.2-ize

--- ckanext/webhooks/actions.py
+++ ckanext/webhooks/actions.py
@@ -1,9 +1,8 @@
 import db
 import logging
 
-from ckan.plugins.toolkit import get_validator, ValidationError
 from ckan.lib.dictization import table_dictize
-from ckan.logic import NotFound, check_access
+from ckan.logic import NotFound, check_access, ValidationError
 
 import ckan.lib.navl.dictization_functions as df
 import ckan.model as model
@@ -11,19 +10,19 @@ import ckan.model as model
 log = logging.getLogger(__name__)
 
 schema = {
-    'id': [get_validator('ignore_empty'), unicode],
-    'address': [get_validator('not_empty'), unicode],
-    'topic': [get_validator('not_empty'), unicode],
-    'user_id': [get_validator('ignore_missing'), unicode],
-    'created_at': [get_validator('ignore_missing'), get_validator('isodate')]
+    'id': ['ignore_empty', unicode],
+    'address': ['not_empty', unicode],
+    'topic': ['not_empty', unicode],
+    'user_id': ['ignore_missing', unicode],
+    'created_at': ['ignore_missing', 'isodate']
 }
 
 schema_get = {
-    'id': [get_validator('not_empty'), unicode]
+    'id': ['not_empty', unicode]
 }
 
 schema_list = {
-    'topic': [get_validator('not_empty'), unicode]
+    'topic': ['not_empty', unicode]
 }
 
 def webhook_create(context, data_dict):
